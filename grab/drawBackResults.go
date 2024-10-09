459468fbf033b27c39154bb78d1a90961645929f
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2024-10-09 13:16:22 +0200

    removed debugs

--- grab/drawBackResults.go
+++ grab/drawBackResults.go
@@ -10,14 +10,16 @@ import (
 
 func (this *GrabcameraClass) drawBackResults() {
 
-	for i := 0; i < len(this.debugMarkList); i++ {
-		//playGround
-		if this.debugMarkList[i].Checked {
-			gocv.Circle(&this.playGround, this.debugMarkList[i].Point, this.debugMarkList[i].Pixelsize, color.RGBA{100, 100, 100, 120}, int(1.0*this.pixelScale))
-		} else {
-			gocv.Circle(&this.playGround, this.debugMarkList[i].Point, this.debugMarkList[i].Pixelsize, color.RGBA{100, 100, 100, 120}, int(2.0*this.pixelScale))
+	/*
+		for i := 0; i < len(this.debugMarkList); i++ {
+			//playGround
+			if this.debugMarkList[i].Checked {
+				gocv.Circle(&this.playGround, this.debugMarkList[i].Point, this.debugMarkList[i].Pixelsize, color.RGBA{100, 100, 100, 120}, int(1.0*this.pixelScale))
+			} else {
+				gocv.Circle(&this.playGround, this.debugMarkList[i].Point, this.debugMarkList[i].Pixelsize, color.RGBA{100, 100, 100, 120}, int(2.0*this.pixelScale))
+			}
 		}
-	}
+	*/
 
 	// Drawing back the results
 	for i := 0; i < len(this.checkMarkList); i++ {
